74e603c5c1b424beaa74fc44dbc058256880b09e
Lukas Schaefer <luki.schaefer96@gmail.com>
2017-02-05 12:59:44 +0100

    add aliases

---
+++ aliases.fish
@@ -0,0 +1,35 @@
+# ----------------------------------------------------------------
+# directory movement
+# ----------------------------------------------------------------
+alias ... 'cd ../..'
+
+# ----------------------------------------------------------------
+# Git
+# ----------------------------------------------------------------
+alias g 'git'
+alias ga 'git add'
+alias gaa 'git add --all'
+alias gc 'git commit -v'
+alias gclean 'git clean -fd'
+alias gco 'git checkout'
+alias gcm 'git checkout master'
+alias gcmsg 'git commit -m'
+alias gd 'git diff'
+alias gf 'git fetch'
+alias gl 'git pull'
+alias glg 'git log --stat'
+alias gm 'git merge'
+alias gss 'git status -s'
+
+alias gp 'git push'
+
+alias gcl 'git clone'
+
+# ---------------------------------------------------------------
+# Useful Parameters
+# ---------------------------------------------------------------
+
+# enable interactive mode for mv, rm, cp (asks before overwriting)
+alias rm 'rm -i'
+alias cp 'cp -i'
+alias mv 'mv -i'
